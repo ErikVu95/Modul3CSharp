b3de46d3d66151c6e785fddc07d83aa0414ab664
Erik <101059428+ErikVu95@users.noreply.github.com>
2022-08-12 14:09:57 +0200

    Test upload

---
+++ IfElseOppgave2/IfElseOppgave2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Xml.Schema;
+
+namespace IfElseOppgave2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+
+            var num1 = random.Next(1, 4);
+            var num2 = random.Next(1, 4);
+            int total = 0;
+
+
+            var b = NumberPlusOrMultiply(num1, num2);
+
+            Console.WriteLine($"{num1}");
+            Console.WriteLine($"{num2}");
+            Console.WriteLine($"{b}");
+
+        }
+
+        public static int NumberPlusOrMultiply(int num1, int num2)
+        {
+            if (num1 == num2)
+            {
+                int total = num1 * num2;  
+                return total;
+            }
+            else
+            {
+                int total = num1 + num2;
+                return total;
+            }
+
+        }
+    }
+}
---
+++ IfElseOppgave3/IfElseOppgave3/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace IfElseOppgave3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+
+            //var num1 = random.Next(1, 4);
+            //var num2 = random.Next(1, 4);
+            var num1 = 30;
+            var num2 = 1;
+            //int total = 0;
+
+
+            bool b = NumberPlusOrMultiply(num1, num2);
+
+            Console.WriteLine($"{num1}");
+            Console.WriteLine($"{num2}");
+            Console.WriteLine($"{b}");
+
+        }
+
+        public static bool NumberPlusOrMultiply(int num1, int num2)
+        {
+            if (num1 + num2 == 30)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
