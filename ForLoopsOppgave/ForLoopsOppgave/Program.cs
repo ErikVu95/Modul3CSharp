642ce727ebdc47094f7188b52bd10b5e28811d19
Erik <101059428+ErikVu95@users.noreply.github.com>
2022-08-12 14:15:28 +0200

    test 2

---
+++ ForLoopsOppgave/ForLoopsOppgave/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace ForLoopsOppgave
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var roundNumber = 1;
+
+            while (roundNumber < 10)
+            {
+                Console.WriteLine($"Round number {roundNumber}");
+                roundNumber++;
+            }
+        }
+    }
+}
+
+
+//namespace ForLoopsOppgave
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            string word = "Academy";
+
+//            foreach (char oneLetter in word)
+//            {
+//                Console.WriteLine(oneLetter);
+//            }
+
+//            //(int i = 0; i < word.Length; i++)
+//            //{
+//            //    Console.WriteLine(word[i]);
+//            //}
+
+//        }
+//    }
+//}
+
+
+
+/*Syntax for å lage array i C#.
+namespace ForLoopsOppgave
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array1 = { "hei", "på", "deg" };
+
+            foreach (string oneLetter in array1)
+            {
+                Console.WriteLine(oneLetter);
+            }
+
+            //(int i = 0; i < word.Length; i++)
+            //{
+            //    Console.WriteLine(word[i]);
+            //}
+
+        }
+    }
+}*/
---
+++ ifElseOppgave/ifElseOppgave/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ifElseOppgave
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+
+            var num1 = random.Next(1, 4);
+            var num2 = random.Next(1, 4);
+
+
+            bool b = NumberCheck(num1, num2);
+            
+            if (b)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("not yes");
+            }
+
+        }
+
+        public static bool NumberCheck(int num1, int num2)
+        {
+            if (num1 == num2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
